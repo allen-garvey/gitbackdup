73046268f6d2a87e1d4e46a78f1888a779b3d6ad
Allen Garvey <allen.garvey@gmail.com>
2017-03-03 17:15:14 -0500

    Add pagination traversal support when retrieving data from GitHub api

--- src/github.d
+++ src/github.d
@@ -1,23 +1,62 @@
 module gitbackdup.github;
 import std.uri;
+import std.string;
 import std.net.curl;
+import std.process;
 import std.json;
 import std.stdio;
+import std.conv;
+import std.regex;
+import std.algorithm.searching;
 
-string repositoryUrlFor(string username){
-	return "https://api.github.com/users/" ~ encodeComponent(username) ~ "/repos?per_page=100";
+string repositoryUrlFor(string username, int pageNum=1){
+	return "https://api.github.com/users/" ~ encodeComponent(username) ~ "/repos?per_page=100&page=" ~ to!string(pageNum);
+}
+
+int pagesFor(string responseHeader){
+	string linkField = null;
+	//look for link field in response
+	foreach(string line; splitLines(responseHeader)){
+		if(startsWith(line, "Link:")){
+			linkField = line;
+			break;
+		}
+	}
+	//no link field means there is only one page
+	if(linkField == null){
+		return 1;
+	}
+	//find page number of last page
+	auto m = matchAll(linkField, regex(`page=\d+`));
+	string result;
+	while(!m.empty){
+		result = m.front.hit;
+		m.popFront();
+	}
+
+	result = replaceAll(result, regex(`[^\d]`), "");
+
+	return to!int(result);
+}
+
+int repositoryPagesFor(string username){
+	auto headRequest = execute(["curl", "--silent", "-I", repositoryUrlFor(username)]);
+	return pagesFor(headRequest.output);
+
 }
 
 
 string[] reposFor(string username){
-	auto content = get(repositoryUrlFor(username));
-	JSONValue response = parseJSON(content);
-
-	JSONValue[] repos = response.array;
 	string[] repoUrls;
-	
-	foreach(JSONValue repo; repos){
-		repoUrls ~= repo["git_url"].str;
+	int pages = repositoryPagesFor(username);
+
+	for(int pageNum=1;pageNum<=pages;pageNum++){
+		auto content = get(repositoryUrlFor(username, pageNum));
+		JSONValue response = parseJSON(content);
+		JSONValue[] repos = response.array;
+		foreach(JSONValue repo; repos){
+			repoUrls ~= repo["git_url"].str;
+		}
 	}
 
 	return repoUrls;
